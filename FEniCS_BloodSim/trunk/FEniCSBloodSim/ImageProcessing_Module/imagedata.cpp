466bffc6438ef764541fbaee7655b7a0c32e7754
calonso <calonso@8de869db-81d1-4a97-8395-e865795caa11>
2016-04-14 16:09:58 +0000

    The following elements were added: + Added the basic functionality for opening a 3D image + Added the basic functionality for opening a 2D slice + Added basically functionality to remove images and keep all the Qvtk objects in black + Adjusted the design of the other tabs: Fenics, mesh, simulation and visualization

--- FEniCS_BloodSim/trunk/FEniCSBloodSim/ImageProcessing_Module/imagedata.cpp
+++ FEniCS_BloodSim/trunk/FEniCSBloodSim/ImageProcessing_Module/imagedata.cpp
@@ -3,10 +3,12 @@
 #include "ImageProcessing_Module/imagereader.h"
 ImageData::ImageData()
 {
-      _imData3D = ImageType3D::New();
+    //  _imData3D = ImageType3D::New();
+      _imVolumeData = vtkVolume::New();
+      _imageData = vtkImageData::New();
 }
 
-
+/*
 ImageType3D::ConstPointer ImageData::getImData3D()
 {
     return _imData3D;
@@ -15,6 +17,29 @@ ImageType3D::ConstPointer ImageData::getImData3D()
 void ImageData::setImData3D(ImageType3D::ConstPointer imPointer)
 {
     _imData3D = imPointer;
+}
+*/
+
+
+vtkVolume * ImageData::getVolumeData()
+{
+    return _imVolumeData;
+}
+
+void ImageData::setVolumeData(vtkVolume * volPointer)
+{
+    _imVolumeData = volPointer;
+}
+
+
+vtkImageData * ImageData::getImageData()
+{
+    return _imageData;
+}
+
+void ImageData::setImageData(vtkImageData * imData)
+{
+    _imageData = imData;
 }
 
 
--- FEniCS_BloodSim/trunk/FEniCSBloodSim/ImageProcessing_Module/imagereader.cpp
+++ FEniCS_BloodSim/trunk/FEniCSBloodSim/ImageProcessing_Module/imagereader.cpp
@@ -1,5 +1,13 @@
 #include "imagereader.h"
 
+#include <QFileInfo>
+#include <vtkVolume.h>
+#include <vtkSmartVolumeMapper.h>
+#include <vtkVolumeProperty.h>
+#include <vtkSmartPointer.h>
+#include <vtkImageReader2Factory.h>
+#include <vtkImageReader2.h>
+#include <vtkNIFTIImageReader.h>
 
 // To read  & Write images in 2D and 3D
 
@@ -12,9 +20,46 @@ void ImageReader::readImage(ImageData * imageData,QString imPath)
     if ((imPath.isEmpty()) || (imPath.isNull()))
         return;
 
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileName(imPath.toStdString());
-    reader->Update();
-    imageData->setImData3D(reader->GetOutput());
+
+    bool _isNIIFile = false;
+    QString _fileSuffix = QFileInfo(imPath).completeSuffix();
+    if (_fileSuffix.contains("nii"))
+        _isNIIFile = true;
+
+    // Create our volume and mapper
+    vtkVolume *volume = vtkVolume::New();
+    vtkSmartVolumeMapper *mapper = vtkSmartVolumeMapper::New();
+    // Create the property and attach the transfer functions
+    vtkVolumeProperty *property = vtkVolumeProperty::New();
+
+    // Read file
+    if (!_isNIIFile)
+    {
+        vtkSmartPointer<vtkImageReader2Factory> readerFactory = vtkSmartPointer<vtkImageReader2Factory>::New();
+        vtkImageReader2 * reader = readerFactory->CreateImageReader2(imPath.toStdString().c_str());
+        reader->SetFileName(imPath.toStdString().c_str());
+        reader->Update();
+        imageData->setImageData(reader->GetOutput());
+        mapper->SetInputConnection( reader->GetOutputPort() );
+    }else
+    {
+        vtkSmartPointer<vtkNIFTIImageReader> _niftiReader = vtkSmartPointer<vtkNIFTIImageReader>::New();
+        _niftiReader->SetFileName(imPath.toStdString().c_str());
+        _niftiReader->Update();
+        imageData->setImageData(_niftiReader->GetOutput());
+        mapper->SetInputConnection( _niftiReader->GetOutputPort() );
+    }
+
+
+    property->SetInterpolationTypeToLinear();
+
+    // connect up the volume to the property and the mapper
+    volume->SetProperty( property );
+    volume->SetMapper( mapper );
+
+    mapper->SetBlendModeToMaximumIntensity();
+
+    imageData->setVolumeData(volume);
+
 }
 

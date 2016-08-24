4a9c17864733c59782fa238cca93ce585c2131c5
calonso <calonso@8de869db-81d1-4a97-8395-e865795caa11>
2016-08-24 14:49:05 +0000

    + Slice by slice iterator with a pipeline of filters: aniostropic diffusion + binary threshold + erosion

---
+++ FEniCS_BloodSim/trunk/VascularSegmentation/Defines/ImageITKVTKDefines.h
@@ -0,0 +1,51 @@
+#ifndef IMAGEITKVTKDEFINES_H
+#define IMAGEITKVTKDEFINES_H
+
+
+/* ================================================ */
+// Filters
+
+enum TypeFilters{
+    MEDIAN_FILTER  = 0,
+    NO_FILTER_FOUND = -1
+};
+
+/* ================================================ */
+/*                  ITK                             */
+// General defines needed for those classes using ITK
+#include <iostream>
+#include <itkImage.h>
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+
+using namespace std;
+using namespace itk;
+
+enum ImageDimension{
+    Two_Dimensions = 2,
+    Three_Dimensions = 3
+};
+
+typedef Image<unsigned char, Two_Dimensions>  ImageType2D;
+typedef Image<unsigned char, Three_Dimensions>  ImageType3D;
+
+typedef ImageFileReader<ImageType2D> ReaderType2D;
+typedef ImageFileWriter<ImageType2D> WriterType2D;
+
+
+typedef ImageFileReader<ImageType3D> ReaderType3D;
+typedef ImageFileWriter<ImageType3D> WriterType3D;
+
+/* ================================================ */
+
+/* ================================================ */
+/*                  VTK                             */
+// General defines needed for those classes using VTK - GLUE
+
+#include <itkVTKImageToImageFilter.h>
+typedef itk::VTKImageToImageFilter<ImageType2D> VTKImageToImage2DType;
+typedef itk::VTKImageToImageFilter<ImageType3D> VTKImageToImage3DType;
+
+
+
+#endif // IMAGEITKVTKDEFINES_H
